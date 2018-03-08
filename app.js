536bb2df4552a063a048e9ca9ca2590f90b961ee
Alex Kim <alexkim@macs-mbp-2.attlocal.net>
2018-03-07 20:18:02 -0600

    added access control

--- app.js
+++ app.js
@@ -18,6 +18,12 @@ var japanese = {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next()
+});
+
 app.get('/', function(req,res){
   res.render('index');
 })
