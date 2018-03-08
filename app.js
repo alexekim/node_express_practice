c82e0e39ccde5547758307f499f7c23ce40ffdb2
Alex Kim <alexkim@macs-mbp-2.attlocal.net>
2018-03-07 20:28:58 -0600

    Added comment

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ var japanese = {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+//must be specific for route??
 app.all('/japanese', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Origin", "http://www.lung.org");
