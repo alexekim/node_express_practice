f78558b423be880211277dfdfad41714d0d7e188
Alex Kim <alexkim@macs-mbp-2.attlocal.net>
2018-03-07 20:27:48 -0600

    specifically routed for japanese

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var japanese = {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-app.all('/', function(req, res, next) {
+app.all('/japanese', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Origin", "http://www.lung.org");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
